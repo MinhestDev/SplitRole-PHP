c23f9835e71ca12c816e86c4c1f361d066adc2da
TIEN1909 <vothanhtien190901@gmail.com>
2025-04-07 04:49:47 +0700

    feat: roles

--- quanlynhansu/config.php
+++ quanlynhansu/config.php
@@ -1,19 +1,16 @@
 <?php
 
-	$conn = mysqli_connect("localhost", "root", "", "quanly_nhansu");
+$conn = mysqli_connect("127.0.0.1", "root", "", "ql_nhansu");
 
-	if (!$conn) {
-	    echo "Error: Unable to connect to MySQL." . PHP_EOL;
-	    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
-	    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
-	    exit;
-	}
+if (!$conn) {
+	echo "Error: Unable to connect to MySQL." . PHP_EOL;
+	echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+	echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+	exit;
+}
 
-	
-	// Set timezone 
-	date_default_timezone_set('Asia/Ho_Chi_Minh');
-	// set char set
-	mysqli_set_charset($conn, 'utf8');
-	
 
-?>
+// Set timezone 
+date_default_timezone_set('Asia/Ho_Chi_Minh');
+// set char set
+mysqli_set_charset($conn, 'utf8');
